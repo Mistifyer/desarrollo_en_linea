c083d21911e2462c3e623d839701baef0aaed314
Julio Alejandro Hernandez Salvador <119072968+Mistifyer@users.noreply.github.com>
2023-08-02 23:00:56 -0600

    Terminando el archivo Js del Menú

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,3 +4,20 @@ btnMenu.addEventListener('click', function() {
     menu.classList.toggle('mostrar');
 });
 
+const subMenuBtn = document.querySelectorAll('.submenu__btn');
+for(let i=0; i<subMenuBtn.length; i++) {
+    subMenuBtn[i].addEventListener('click', function() {
+        if(window.innerWidth < 1024) {
+            const header__submenu = this.nextElementSibling;
+            const height = header__submenu.scrollHeight;
+
+            if(header__submenu.classList.contains('desplegar')){
+                header__submenu.classList.remove('desplegar');
+                header__submenu.removeAttribute('style');
+            } else {
+            header__submenu.classList.add('desplegar');
+            header__submenu.style.height = height + 'px';
+            }
+        }
+    });
+}
