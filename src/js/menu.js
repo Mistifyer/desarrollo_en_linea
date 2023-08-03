f5673af8c0c36742f0dc92324eced5ab48393665
Julio Alejandro Hernandez Salvador <119072968+Mistifyer@users.noreply.github.com>
2023-08-02 23:11:07 -0600

    Correccion de error ej el JS del Menú

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,5 @@
 const btnMenu = document.querySelector('#btnMenu');
-const menu = document.querySelector(#menu);
+const menu = document.querySelector('#menu');
 btnMenu.addEventListener('click', function() {
     menu.classList.toggle('mostrar');
 });
